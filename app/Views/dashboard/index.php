73a9141dc0005c999ffbe9e9fe7570fe8b36a7dd
NikarashiHatsu <yourlovelydev@gmail.com>
2021-12-19 15:20:39 +0700

    chore: add some deco to index

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -4,4 +4,33 @@
 <div class="bg-white rounded p-6 shadow">
     Selamat datang, Aghits Nidallah
 </div>
+<div class="mt-6 grid grid-cols-3 grid-flow-row gap-6">
+    <div class="col-span-1 flex flex-col justify-center text-center p-6 rounded text-white bg-red-500 shadow-lg shadow-red-500/50">
+        <i class="text-6xl mdi-action-view-carousel"></i>
+        <p>
+            Kamar
+        </p>
+        <p>
+        // Total
+        </p>
+    </div>
+    <div class="col-span-1 flex flex-col justify-center text-center p-6 rounded text-white bg-green-500 shadow-lg shadow-green-500/50">
+        <i class="text-6xl mdi-communication-email"></i>
+        <p>
+            Pasien
+        </p>
+        <p>
+        // Total
+        </p>
+    </div>
+    <div class="col-span-1 flex flex-col justify-center text-center p-6 rounded text-white bg-blue-500 shadow-lg shadow-blue-500/50">
+        <i class="text-6xl mdi-editor-insert-invitation"></i>
+        <p>
+            Dokter
+        </p>
+        <p>
+        // Total
+        </p>
+    </div>
+</div>
 <?= $this->endSection() ?>
